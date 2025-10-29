ea5819b7ce93dbf91ba63e85c779e8c87858fe1e
Ivan <ivantiang@gmail.com>
2025-10-29 18:42:55 +1000

    Refactor Nifti loader to support normalization, resizing, and one-hot mask conversion

--- recognition/ImprovedUNet-46494157/dataset.py
+++ recognition/ImprovedUNet-46494157/dataset.py
@@ -1,5 +1,9 @@
+import os
 import numpy as np
 import nibabel as nib
+import skimage.transform as skTrans
+from torch.utils.data import Dataset
+import torch
 from tqdm import tqdm
 
 def to_channels(arr: np.ndarray, num_classes=5, dtype=np.uint8) -> np.ndarray:
@@ -8,51 +12,45 @@ def to_channels(arr: np.ndarray, num_classes=5, dtype=np.uint8) -> np.ndarray:
         res[..., c][arr == c] = 1
     return res
 
-# load medical image functions
-def load_data_2D(imageNames, normImage=False, getAffines=False, early_stop=False):
-    '''
-    Load medical image data from names, cases list provided into a list for each.
-    This function pre-allocates 4D arrays for conv2d to avoid excessive memory usage.
-
-    normImage : bool (normalise the image 0.0 -1.0)
-    early_stop : Stop loading pre-maturely, leaves arrays mostly empty, for quick loading and testing scripts.
-    '''
+def load_data_2D(imageNames, normImage=False, categorical=False,
+                 dtype=np.float32, getAffines=False, early_stop=False):
     affines = []
-    # get fixed size
     num = len(imageNames)
-    first_case = nib.load(imageNames[0]).get_fdata(caching='unchanged')
+    first_case = nib.load(imageNames[0]).get_fdata(caching="unchanged")
+
     if len(first_case.shape) == 3:
-        first_case = first_case[:, :, 0]  # sometimes extra dims, remove
+        first_case = first_case[:, :, 0]
+
     if categorical:
-        first_case = to_channels(first_case, dtype=dtype)
+        first_case = to_channels(first_case)
         rows, cols, channels = first_case.shape
         images = np.zeros((num, rows, cols, channels), dtype=dtype)
     else:
         rows, cols = first_case.shape
         images = np.zeros((num, rows, cols), dtype=dtype)
 
-    for i, inName in enumerate(tqdm(imageNames)):
-        niftiImage = nib.load(inName)
-        inImage = niftiImage.get_fdata(caching='unchanged')  # read disk only
-        affine = niftiImage.affine
-        if len(inImage.shape) == 3:
-            inImage = inImage[:, :, 0]  # sometimes extra dims in HipMRI_study data
+    for i, inName in enumerate(tqdm(imageNames, desc="Loading NIfTI files")):
+        nifti = nib.load(inName)
+        img = nifti.get_fdata(caching="unchanged")
 
-        inImage = inImage.astype(dtype)
+        if len(img.shape) == 3:
+            img = img[:, :, 0]
+
+        img = skTrans.resize(img, (256, 128), order=0, preserve_range=True)
+
         if normImage:
-            # ~ inImage = inImage / np.linalg.norm(inImage)
-            # ~ inImage = 255. * inImage / inImage.max()
-            inImage = (inImage - inImage.mean()) / inImage.std()
+            mean, std = img.mean(), img.std()
+            img = (img - mean) / std if std > 1e-6 else np.zeros_like(img)
+
         if categorical:
-            inImage = utils.to_channels(inImage, dtype=dtype)
-            images[i, :, :, :] = inImage
+            img = to_channels(img, dtype=dtype)
+            images[i, :, :, :] = img
         else:
-            images[i, :, :] = inImage
-        affines.append(affine)
-        if i > 20 and early_stop:
+            images[i, :, :] = img
+
+        affines.append(nifti.affine)
+
+        if early_stop and i > 20:
             break
 
-    if getAffines:
-        return images, affines
-    else:
-        return images
+    return (images, affines) if getAffines else images
